f183789173b13b7bc4f1ac15daeca2fb482a67df
bryleero <157856731+bryleer15@users.noreply.github.com>
2024-10-16 12:01:26 -0500

    n

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c5837be88f7e1db71708a572084ff1b5421e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23ddce642b94c297222381289c2025e19611f1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
